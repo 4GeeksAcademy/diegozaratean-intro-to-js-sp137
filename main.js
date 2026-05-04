30dd231321a5c634347aa4f8420d728653300119
diego <diegozaratean@gmail.com>
2026-05-04 09:28:55 +0000

    finish js class 1

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('hola desde main')
+
+let nombre = 'cristiano'
+let apellido = "ronaldo"
+let edad = 35.6
+let hijos = 2
+let estaVivo = true
+let mudial = null
+
+// element.     1.       2.         3.     4
+let frutas = ['mango','manzana','pera','sandia']
+// pos.          0.      1.        2.      3
+
+let coche = {marca:'tesla', color:'negro',matricula:'qwer',anio:'2026'}
+
+
