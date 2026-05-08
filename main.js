cfb0b0b95334d51f8f4bff207651c55086fc436f
diego <diegozaratean@gmail.com>
2026-05-08 09:34:10 +0000

    finish js class 3

--- main.js
+++ main.js
@@ -2,104 +2,102 @@ console.log('hola desde main')
 
 
 
-console.log('Quemado')
-console.log(1)
-console.log(2)
-console.log(3)
-console.log(4)
-console.log(5)
+// let alumno = 'Benjamin'
+// console.log('Antes de la funcion' + alumno)
+// function saludarAlumno(alumno){
+//     console.log('Dentro de la funcion' + alumno)
+//     console.log('hola '+alumno)
+// }
+// console.log('Despues de la funcion' + alumno)
 
-// for(inicialiación;condición;actualización)
-// i = 0 ====> 0. 1
-// i = 1 ====> 1. 2 
-// i = 2 ====> 2.  3
-// i = 3 ====> 3. 4
-// i = 4 ====> 4. 5
-// i = 5 ====> 
+// saludarAlumno('Jack')
 
-console.log('Dinamico')
-for(i=1;i <= 5; i= i + 1){
-    console.log(i)
 
+
+// function transportar(origen,destino){
+//     console.log('saliendo de' , origen)
+//     console.log('transportandome')
+//     console.log('llegando a ' + destino)
+// }
+
+// transportar('casa','gimnasio')
+// transportar('gimnasio','ofi')
+// transportar('ofi','casa')
+
+
+// function sumar(num1,num2){
+//     return num1 + num2    
+// }
+
+// console.log(sumar(2,2))
+
+// function autorizarEntrada(edad){
+//     if(edad > 18){
+//         return 'Puedes entrar'
+//     }
+//     console.log('me imprimo')
+//     return 'No entras te queda afuera'
+    
+// }
+
+// console.log(autorizarEntrada(15))
+
+
+
+
+function calculoImpuesto(ingreso){
+    console.log('Debes pagar '+ ingreso*0.2)
+    return ingreso * 0.2
 }
 
+let result = calculoImpuesto(10000)
 
-// imprimir de 20 a 15
-//20
-//19
-//18
-//17
-//16
-//15
-//14
-for (let i = 20; i >= 15; i--) {
-    console.log(i)
+document.getElementById('resultado').innerHTML = result
+
+
+// function sumar(num1,num2){
+//     return num1 + num2
+// }
+
+// console.log(sumar(2,2))
+// console.log(sumar(1,8))
+// console.log(sumar(0,3))
+// console.log(sumar(8,2))
+
+
+
+// console.log('Antes de Window on load')
+// window.onload = function(){
+//     console.log('DENTRO de Window on load')
+// }
+// console.log('Despues de Window on load')
+
+
+// Funcion declarativa
+saludoDeclarativo()
+function saludoDeclarativo(){
+    console.log('saludoDeclarativo')
 }
 
-
-// i = 30 ===> 30
-// i = 35 ===> 35
-// i = 40 ===> 40
-// i = 45 ===> 45
-// i = 50 ===> 50
-// i = 55 ===> 
-
-for(i=30;i <=50;i+=5)
-{
- console.log(i)
+// Función de expresión
+const saludoExpresión = function(){
+    console.log('saludoExpresión')
 }
+saludoExpresión()
 
-// i = i + 1 ===> i++.  i + + 
-// i = i + 5 ===> i+=5  i+= 5
-
-let nombres = ['jhon snow','harvey specter','mr bean']
-
-console.log(nombres)
-nombres.push('patrick jane')
-console.log(nombres)
-
-const alumnos = ['Pascual','Marianela','Julio']
-
-console.log(alumnos)
-alumnos.push('Sergi')
-console.log(alumnos)
-// alumnos = ['Pascual','Marianela','Julio','Sergi']
-
-
-
-
-// lenght           1          2         3
-const frutas = ['manazana','banana','mandarina']
-// position         0           1       2
-
-console.log(frutas)
-//frutas imprmie lo que tienes en la psicion 1
-//   
-console.log(frutas[ 1 ])
-
-frutas.push('pasta')
-console.log(frutas)
-frutas.unshift('pizza')
-console.log(frutas)
-console.log(frutas.pop())
-console.log(frutas)
-frutas.shift()
-console.log(frutas)
-frutas.splice(1,0,'milanesa')
-console.log(frutas)
-frutas.push('hamburguesa')
-
-console.log('Quemado')
-console.log(frutas[0] + 's')
-console.log(frutas[1] + 's')
-console.log(frutas[2] + 's')
-console.log("mi comida favorita son "+frutas[3] + 's  y no me gustan las' + frutas[0])
-
-console.log("mi comida favorita son ${frutas[3]}s y no me gustan las ${frutas[0]}s "  )
-
-// i =0 =====> Array(4) [ "manazana", "milanesa", "banana", "mandarina" ] ====> manazanas
-
-console.log('Dinamico')
-for(let i = 0; i < frutas.length;i++){
-    console.log(frutas[i] + 's')
+// Función Flecha Arrow
+const saludoArrow = ()=>{
+    console.log('saludoArrow')
 }
+saludoArrow()
+
+///// Refactoring ////
+
+// PSEDUDO CODIGO
+// Desarrollar un generador de excusas de Quien, Cuando , que , Accion
+// 1. modificar html desde js
+// 2. tomar array who y obtner un elemento aleatorio
+// 3. tomar array what y obtner un elemento aleatorio
+// 4. tomar array when y obtner un elemento aleatorio
+// 5. tomar array action y obtner un elemento aleatorio
+// 6. unir los vlaores alaetorios en una excusa
